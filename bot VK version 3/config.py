cde448fc1c56b74bb2f9b324479b9e7d819f82ab
Maykan-PRO <57722163+Maykan-PRO@users.noreply.github.com>
2020-04-18 15:56:45 +0300

    Add files via upload

--- bot VK version 3/config.py
+++ bot VK version 3/config.py
@@ -1,3 +1,7 @@
 token = "2453f743a9720b082acd93fbecb88e9fde7a4d737fbd4968fb89d00f54d305ad651ecfafc7f90b0d9f3ae"
 
 sql_baza = "baza.db"
+
+group_id = "182969363"
+
+admin_password = "bashmak010infuzoria"

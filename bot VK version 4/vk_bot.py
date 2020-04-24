ff15ad5944f5c4935a271f42f221c58a5044443f
Maykan-PRO <57722163+Maykan-PRO@users.noreply.github.com>
2020-04-25 01:42:07 +0300

    Add files via upload

--- bot VK version 4/vk_bot.py
+++ bot VK version 4/vk_bot.py
@@ -2,12 +2,14 @@ import bs4 as bs4
 import requests, random
 import pyowm
 
+
 class VkBot:
     def __init__(self, user_id):
         print("\nСоздан объект бота!")
         self._USER_ID = user_id
         self._USERNAME = self._get_user_name_from_vk_id(user_id)
-        self._COMMANDS = ["ПРИВЕТ", "!ПОГОДА", "!ВРЕМЯ", "ПОКА", "!КОМАНДЫ", "!ЧИСЛО", '!МОНЕТКА', 'РЕГИСТРАЦИЯ КИЛЛЕР', '!ЦИТАТА']
+        self._COMMANDS = ["ПРИВЕТ", "!ПОГОДА", "!ВРЕМЯ", "ПОКА", "!КОМАНДЫ", "!ЧИСЛО", '!МОНЕТКА', 'РЕГИСТРАЦИЯ КИЛЛЕР',
+                          '!ЦИТАТА', "ИГРА ПРИКЛЮЧЕНИЕ"]
 
     def _get_user_name_from_vk_id(self, user_id):
         request = requests.get("https://vk.com/id" + str(user_id))
@@ -31,12 +33,12 @@ class VkBot:
             return f"Пока-пока, {self._USERNAME}!"
         elif message.upper() == self._COMMANDS[4]:
             return f"Список команд:\n" \
-                    f"\n" \
-                    f"!Погода - для просмотра погоды в Москве &#9728;\n" \
+                   f"\n" \
+                   f"!Погода - для просмотра погоды в Москве &#9728;\n" \
                    f"!Время - для просмотра времени &#127763;\n" \
                    f"!Число [от]   [до] - генерация рандомного числа &#128290;\n" \
                    f"!Монетка - подбросить монетку (орел/решка) &#127922;\n" \
-                    f"!Цитата - мудрая цитата ☝"
+                   f"!Цитата - мудрая цитата ☝"
         elif message.upper()[0:6] == self._COMMANDS[5]:
             if len(message.split(' ')) != 3:
                 return f"Неверный синтаксис, надо вводить так:\n" \
@@ -50,11 +52,16 @@ class VkBot:
             return f"Иии выпадает! " + random.choice(['Орёл', 'Решка']) + ' ' + str(
                 random.choice(['&#127773;', '&#128516;', '&#128527;', '&#128517;']))
         elif message.upper() == self._COMMANDS[7]:
-            return "Отлично, для регитрации на игру отправьте свою фотографию &#128100;, чтобы другие игроки смогли вас найти &#8986;"
+            return "Отлично, для регитрации на игру отправьте свою фотографию &#128100;, чтобы другие игроки смогли вас" \
+                   " найти &#8986;"
         elif message.upper() == self._COMMANDS[8]:
             with open('citata.txt', 'r') as file:
                 line = file.readlines()
                 return line[random.randint(0, 9)].encode("utf-8")
+        elif message.upper() == self._COMMANDS[9]:
+            return "Вы начали игру {название еще не придкмал}, для выхода из игры напишите: выход игра приключение," \
+                   " для сохранения игры напишите: сохранить игра приключение, для загрузки сохранения напишите:" \
+                   " загрузить игра приключение."
         else:
             return "Не понимаю о чем вы... &#128530;\n Для списка команд напишите:\n" \
                    "!команды"
@@ -86,8 +93,9 @@ class VkBot:
 
     @staticmethod
     def _get_weather(city=str("Москва")) -> list:
-        owm = pyowm.OWM('6d00d1d4e704068d70191bad2673e0cc', language = 'ru')
+        owm = pyowm.OWM('6d00d1d4e704068d70191bad2673e0cc', language='ru')
         place = city
         observation = owm.weather_at_place(place)
         w = observation.get_weather()
-        return 'В городе ' + city + ' сейчас ' + w.get_detailed_status() + ' и ' + str(w.get_temperature('celsius')['temp']) + ' градусов по Цельсию.'
+        return 'В городе ' + city + ' сейчас ' + w.get_detailed_status() + ' и ' + str(
+            w.get_temperature('celsius')['temp']) + ' градусов по Цельсию.'
